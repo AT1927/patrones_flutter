346c9208fefd288f4c46484aa15f2eca00988765
AT1927 <atorresmunoz88@gmail.com>
2025-02-18 10:25:07 -0500

    Finally injections dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
-import 'package:clean_architecture/data/repositories/volcano_repository_impl.dart';
-import 'package:clean_architecture/domain/usecases/delete_volcano.dart';
-import 'package:clean_architecture/domain/usecases/get_all_volcanos.dart';
-import 'package:clean_architecture/domain/usecases/save_volcano.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:clean_architecture/injection.dart';
 import 'package:clean_architecture/presentation/bloc/volcano_bloc.dart';
 import 'package:clean_architecture/presentation/bloc/volcano_event.dart';
 import 'package:clean_architecture/presentation/pages/volcano_list_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+/*import 'package:clean_architecture/data/repositories/volcano_repository_impl.dart';
+import 'package:clean_architecture/domain/usecases/delete_volcano.dart';
+import 'package:clean_architecture/domain/usecases/get_all_volcanos.dart';
+import 'package:clean_architecture/domain/usecases/save_volcano.dart';*/
 
 void main() {
+  initializeDependencies();
   runApp(const MyApp());
 }
 
@@ -17,12 +19,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Crear instancias de las dependencias
-    final volcanoRepository = VolcanoRepositoryImpl();
-    final getAllVolcanos = GetAllVolcanos(volcanoRepository);
-    final saveVolcano = SaveVolcano(volcanoRepository);
-    final deleteVolcano = DeleteVolcano(volcanoRepository);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Practica Patrones Flutter - Torres',
@@ -31,11 +27,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
       ),
       home: BlocProvider(
-        create: (context) => VolcanoBloc(
-          getAllVolcanos: GetAllVolcanos(volcanoRepository),
-          saveVolcano: SaveVolcano(volcanoRepository),
-          deleteVolcano: DeleteVolcano(volcanoRepository),
-        )..add(LoadVolcanos()),
+        create: (context) => getIt<VolcanoBloc>()..add(LoadVolcanos()),
         child: const VolcanoListPage(),
       ),
     );
